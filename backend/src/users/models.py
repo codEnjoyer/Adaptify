4296b5c676f147b0853ef1017a7476590bca7e70
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-10-30 16:26:08 +0500

    Дополняет модель пользователя

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -1,8 +1,10 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column
 
 from database import BaseModel
 from roles import UserRoles
 
 
 class User(SQLAlchemyBaseUserTableUUID, BaseModel):
-    role: UserRoles = UserRoles.Employee
+    role: Mapped[UserRoles] = mapped_column(Enum, nullable=False, default=UserRoles.Employee)
