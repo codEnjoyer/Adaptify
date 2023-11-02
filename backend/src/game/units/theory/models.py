1232a4d8cf50cb1734dfc5f8c7576f2401caf21f
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-11-02 16:19:53 +0500

    Добавляет основу для модели теории

---
+++ backend/src/game/units/theory/models.py
@@ -0,0 +1,26 @@
+import uuid
+
+from sqlalchemy import UUID, Boolean, Integer, Text, String, URL
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy_utils import URLType
+
+from database import BaseModel
+
+
+class Theory(BaseModel):
+    __tablename__ = 'theory_blocks'
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    theme: Mapped[str] = mapped_column(String, nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False, default="Текст теории")
+
+    videos: Mapped[list["TheoryVideo"]] = relationship(back_populates="theory_block")
+
+
+class TheoryVideo(BaseModel):
+    __tablename__ = 'theory_videos'
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    url: Mapped[URL] = mapped_column(URLType, nullable=False)
+
+    theory: Mapped[Theory] = relationship(back_populates="theory_video")
