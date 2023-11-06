1b116e1efcc21628303d8f7bfe55d6780dd66bb0
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-11-06 22:40:31 +0500

    Изменяет имена переменных окружения так, чтобы они соответствовали тем, что требуются при создании контейнера с postgres

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -8,11 +8,11 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_PASS = os.environ.get("DB_PASS")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
+DB_USER = os.environ.get("POSTGRES_USER")
+DB_PASS = os.environ.get("POSTGRES_PASSWORD")
+DB_HOST = os.environ.get("POSTGRES_DB_HOST")
+DB_PORT = os.environ.get("POSTGRES_DB_PORT")
+DB_NAME = os.environ.get("POSTGRES_DB")
 
 
 class Settings(BaseSettings):
