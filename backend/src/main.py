a80a4460748a47d97f62fc8bd917eec0bdb290ee
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-11-13 18:13:55 +0500

    Добавляет эндпоинты в приложение

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+
+from game.levels.router import router as levels_router
+from game.modules.router import router as modules_router
+from game.map.router import router as map_router
+from game.units.tasks.router import router as tasks_router
+from game.units.theory.router import router as theory_router
+
+from users.router import router as users_router
+from users.tutors.router import router as tutors_router
+from users.employees.router import router as employees_router
 
 app = FastAPI(title="Adaptify")
+
+
+def include_routers(routers: list[APIRouter]) -> None:
+    for router in routers:
+        app.include_router(router)
 
 
 @app.get("/")
 async def root() -> str:
     return "Hello, world!"
-    return 123
+
+
+include_routers([
+    levels_router,
+    modules_router,
+    map_router,
+    tasks_router,
+    theory_router,
+    users_router,
+    tutors_router,
+    employees_router,
+])
