88976d3166cf52cac586cd7c5f0dbfa600bca7f0
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-11-16 16:14:36 +0500

    Исправляет проверку id с числового на uuid

--- backend/src/auth/manager.py
+++ backend/src/auth/manager.py
@@ -1,11 +1,11 @@
 import uuid
 
-from fastapi_users import BaseUserManager, IntegerIDMixin
+from fastapi_users import BaseUserManager, UUIDIDMixin
 
 from users.models import User
 
 
 # TODO: Добавить свой функционал по необходимости
 # https://fastapi-users.github.io/fastapi-users/12.1/configuration/user-manager/
-class UserManager(IntegerIDMixin, BaseUserManager[User, uuid.UUID]):
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     pass
