e5f2127f957175521261366e4b1c0ebd1a195364
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-11-18 21:00:43 +0500

    Слегка улучшает код

--- backend/src/utils/types.py
+++ backend/src/utils/types.py
@@ -3,12 +3,16 @@ from typing import Annotated
 from fastapi import Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-# from auth.base_config import current_user
+from auth.base_config import current_user, current_superuser
 from database import get_async_session
+from game.dependencies import map_service
+from services.map_service import MapService
 from users.models import User
 
-# CurrentUser = Annotated[User, Depends(current_user)]
+CurrentUser = Annotated[User, Depends(current_user)]
+CurrentSuperuser = Annotated[User, Depends(current_superuser)]
 
+MapServiceType = Annotated[MapService, Depends(map_service)]
 AsyncDBSession = Annotated[AsyncSession, Depends(get_async_session)]
 QueryDBLimit = Annotated[int, Query(ge=0, le=10 ** 3)]
 QueryDBOffset = Annotated[int, Query(ge=0, le=10 ** 6)]
