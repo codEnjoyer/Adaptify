e67892f99992d9adddab11d706038049a60f7f15
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-11-20 20:58:06 +0500

    Добавляет API для работы с кураторами

--- backend/src/users/tutors/router.py
+++ backend/src/users/tutors/router.py
@@ -2,21 +2,38 @@ from uuid import UUID
 
 from fastapi import APIRouter
 
-from users.tutors.schemas import TutorRead
+from users.tutors.schemas import TutorRead, TutorCreate, TutorUpdate
+from utils.types import TutorServiceType
 
-router = APIRouter(prefix="/tutors", tags=["Tutor", "User"])
-
-
-@router.get("/")
-async def root() -> list[TutorRead]:
-    return []
+router = APIRouter(tags=["Tutor", "User"])
 
 
-@router.get("/{id}")
-async def get_tutor(id: UUID) -> TutorRead:
-    return TutorRead()
+@router.get("/tutors/", tags=["Dev"])
+async def root(tutor_service: TutorServiceType) -> list[TutorRead]:
+    return await tutor_service.get_all()
 
 
-@router.delete("/{id}")
-async def delete_tutor(id: UUID) -> TutorRead:
-    return TutorRead()
+@router.get("/tutors/{id}/")
+async def get_tutor(id: UUID,
+                    tutor_service: TutorServiceType) -> TutorRead:
+    return await tutor_service.get_one(id)
+
+
+@router.post("/tutors/")
+async def post_tutor(id: UUID,
+                     tutor_create: TutorCreate,
+                     tutor_service: TutorServiceType) -> TutorRead:
+    return await tutor_service.create_one(id, tutor_create)
+
+
+@router.delete("/tutors/{id}/")
+async def delete_tutor(id: UUID,
+                       tutor_service: TutorServiceType) -> TutorRead:
+    return await tutor_service.delete_one(id)
+
+
+@router.patch("/tutors/{id}/")
+async def update_tutor(id: UUID,
+                       tutor_update: TutorUpdate,
+                       tutor_service: TutorServiceType) -> TutorRead:
+    return await tutor_service.update_one(id, tutor_update)
