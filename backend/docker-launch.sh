e107fb7e7cc5ceec090ebfbfb30ff37331c40c81
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-12-17 13:03:00 +0500

    Обновляет головную миграцию

--- backend/docker-launch.sh
+++ backend/docker-launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # При запуске стоит указывать здесь номер последней миграции
 alembic revision --autogenerate
-alembic upgrade head || alembic upgrade 1d75241f340c
+alembic upgrade head || alembic upgrade ee38e6a7803e
 cd src || exit
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:"$BACK_APP_PORT" --timeout 300
